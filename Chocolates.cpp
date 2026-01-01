f7d6e2b4563eb1582219ef7eda6c4e478eb5172f
Ayush <ayushkumares@gmail.com>
2026-01-01 10:44:40 +0530

    Corrected my solution

--- Chocolates.cpp
+++ Chocolates.cpp
@@ -15,50 +15,64 @@ int main()
         v[i] = -1 * v[i];
     }
     long long currSum = 0, maxSum = LLONG_MIN;
-    int st = 0, end = 0;
-    int smallestLen = n;
     for (int i = 0; i < n; i++)
     {
-        long long one = currSum + v[i];
-        long long two = v[i];
-        currSum = max(one, two);
-        if (currSum == one)
-        {
-            end = i;
-        }
-        else
-        {
-            st = i;
-            end = i;
-        }
-        if (currSum > maxSum)
-        {
-            maxSum = currSum;
-            smallestLen = end - st + 1;
-        }
-        else if (currSum == maxSum)
-        {
-            smallestLen = min(smallestLen, end - st + 1);
-        }
-        if (currSum < 0)
-        {
-            currSum = 0;
-            st = i + 1;
-            end = i + 1;
-        }
+        currSum = max(currSum, 0LL);
+        currSum += v[i];
+        maxSum = max(maxSum, currSum);
     }
-    // cout << maxSum << " " << smallestLen << "\n";
+    int len = n;
+    // cout << maxSum << "\n";
+    map<long long, int> mp;
+    long long sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += v[i];
+        if (mp.count(sum - maxSum) != 0)
+        {
+            len = min(len, i - mp[sum - maxSum]);
+        }
+        mp[sum] = i;
+    }
     if (maxSum <= 0)
     {
         cout << n << " " << totalSum << "\n";
     }
-    else if (smallestLen == n)
+    else if (len == n)
     {
-        cout << 1 << " " << -1 * min(v[0], v[n - 1]) << "\n";
+        int tgt = min(v[0], v[n - 1]);
+        int cnt1 = 0, cnt2 = 0;
+        long long tot = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (tot + v[i] <= tgt)
+            {
+                tot += v[i];
+                cnt1++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        tot = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (tot + v[i] <= tgt)
+            {
+                tot += v[i];
+                cnt2++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        cout << max(cnt1, cnt2) << " " << -1 * tgt << "\n";
     }
     else
     {
-        cout << n - smallestLen << " " << totalSum + maxSum << "\n";
+        cout << n - len << " " << totalSum + maxSum << "\n";
     }
 
     return 0;
